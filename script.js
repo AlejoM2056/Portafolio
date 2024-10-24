de0beea3fd0ca55ec2129965c528370ab001cda4
Alejomdi193 <mdiegoalejandro17@gmail.com>
2024-10-24 00:39:44 -0500

    Finish

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ document.addEventListener('DOMContentLoaded', function() {
         escribirTexto();
     });
 });
+
+
+
+    function acomodarNombre() {
+        const profileName = document.getElementById('profile-name');
+        const profileInfo = document.querySelector('.profile-info');
+        
+        if (window.innerWidth >= 768) {
+            
+            profileInfo.prepend(profileName); 
+        } else {
+            const profileHeader = document.querySelector('.profile-header');
+            profileHeader.prepend(profileName); 
+        }
+    }
+
+   
+    acomodarNombre();
+
+ 
+    window.addEventListener('resize', acomodarNombre);
+
